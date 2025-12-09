8d10069309b3a386743bb67e738344d3d920ff1b
FakeKase <turbonoppawit@gmail.com>
2025-12-10 00:00:15 +0700

    read file as matrix dai laew

--- src/VeeBoSolver/Main.java
+++ src/VeeBoSolver/Main.java
@@ -1,31 +1,34 @@
 package VeeBoSolver;
-import VeeBoSolver.DataStructure.*;
-import VeeBoSolver.Solver.*;
 import VeeBoSolver.filereader.*;
+import java.util.Arrays;
 
 class Main
 {
     public static void main(String[] args)
     {
-        MazeData md = MazeGraphReader.readMazeAsGraph("./MazeFiles/m24_20.txt");
-        Graph g = md.graph;
-        System.out.println("StartID = " + g.getStartID());
-        System.out.println("GoalID = " + g.getGoalID());
-        g.PrintMaze(md);
+        int[][] grid = Reader.buildMatrix("./MazeFiles/m15_15.txt");
+        for(int[] i : grid)
+        {
+            System.out.println(Arrays.toString(i));
+        }
+        // Graph g = md.graph;
+        // System.out.println("StartID = " + g.getStartID());
+        // System.out.println("GoalID = " + g.getGoalID());
+        // g.PrintMaze(md);
 
-        Astar solver = new Astar();
-        Long ans = solver.solve(g, g.getStartID(), g.getGoalID());
-        System.out.println("\nMin distance(Astar): "+ ans);
-        g.PrintPath(md, g.getPath());
+        // Astar solver = new Astar();
+        // Long ans = solver.solve(g, g.getStartID(), g.getGoalID());
+        // System.out.println("\nMin distance(Astar): "+ ans);
+        // g.PrintPath(md, g.getPath());
 
-        Dijkstra solveDijkstra = new Dijkstra();
-        Long ansDijkstra = solveDijkstra.dijkstra(g, g.getStartID(), g.getGoalID());
-        System.out.println("\nMin distance: "+ ansDijkstra);
-        g.PrintPath(md, g.getPath());
+        // Dijkstra solveDijkstra = new Dijkstra();
+        // Long ansDijkstra = solveDijkstra.dijkstra(g, g.getStartID(), g.getGoalID());
+        // System.out.println("\nMin distance: "+ ansDijkstra);
+        // g.PrintPath(md, g.getPath());
 
-        Greedy solveGreedy = new Greedy();
-        Long ansGreedy = solveGreedy.greedy(g, g.getStartID(), g.getGoalID());
-        System.out.println("\nMin distance(Greedy): "+ ansGreedy);
-        g.PrintPath(md, g.getPath());
+        // Greedy solveGreedy = new Greedy();
+        // Long ansGreedy = solveGreedy.greedy(g, g.getStartID(), g.getGoalID());
+        // System.out.println("\nMin distance(Greedy): "+ ansGreedy);
+        // g.PrintPath(md, g.getPath());
     }
 }
--- src/VeeBoSolver/filereader/Reader.java
+++ src/VeeBoSolver/filereader/Reader.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class MazeGraphReader {
+public class Reader {
 
     public static MazeData readMazeAsGraph(String filePath) {
 
@@ -68,6 +68,72 @@ public class MazeGraphReader {
         }
 
         return new MazeData(graph, nodeGrid, rows, R, C);
+    }
+
+    public static int[][] buildMatrix(String filepath)
+    {
+        String[][] gridString = readMazeAsMatrix(filepath);
+        int R = gridString.length;
+        int C = gridString[0].length;
+        int[][] weightGrid = new int[R][C];
+        for (int r = 0; r < R; r++) 
+        {
+            for (int c = 0; c < C; c++) 
+            {
+                String cell = gridString[r][c];
+                switch (cell) 
+                {
+                case "#":
+                    weightGrid[r][c] = -1;
+                    break;
+                case "S":
+                    weightGrid[r][c] = 0;
+                    break;
+                case "G":
+                    weightGrid[r][c] = 0;
+                    break;
+                default:
+                    weightGrid[r][c] = Integer.parseInt(cell.replace("\"", ""));
+                    break;
+                }
+            }
+        }
+
+        return weightGrid;
+    }
+
+    private static String[][] readMazeAsMatrix(String filePath) {
+
+        List<List<String>> rows = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty()) continue;
+
+                Pattern cellPattern = Pattern.compile("#|S|G|\"\\d+\"|\\d");
+                Matcher m = cellPattern.matcher(line);
+                List<String> tokens = new ArrayList<>();
+                while (m.find()) tokens.add(m.group());
+                rows.add(tokens);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int R = rows.size();
+        int C = rows.get(0).size();
+
+       String[][] grid = new String[R][C];
+       for(int r = 0; r < R; r++)
+       {
+            for (int c = 0; c < C; c++) 
+            {
+                grid[r][c] = rows.get(r).get(c);
+            }
+       }
+
+        return grid;
     }
 
     private static int parseWeight(String token) {
